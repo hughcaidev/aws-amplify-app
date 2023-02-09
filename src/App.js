5e1b1ed794f4ffd061e5b8e971389b492c0edc3b
Hugh Cai <hughcaidev@gmail.com>
2023-02-09 19:43:42 +0000

    add storage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import logo from "./logo.svg"
 import React, { useState, useEffect } from "react"
 import "./App.css"
 import "@aws-amplify/ui-react/styles.css"
-import { API } from "aws-amplify"
+import { API, Storage } from "aws-amplify"
 import {
     Button,
     Flex,
     Heading,
+    Image,
     Text,
     TextField,
     View,
@@ -32,16 +33,28 @@ const App = ({ signOut }) => {
     async function fetchNotes() {
         const apiData = await API.graphql({ query: listNotes })
         const notesFromAPI = apiData.data.listNotes.items
+        await Promise.all(
+            notesFromAPI.map(async (note) => {
+                if (note.image) {
+                    const url = await Storage.get(note.name)
+                    note.image = url
+                }
+                return note
+            })
+        )
         setNotes(notesFromAPI)
     }
 
     async function createNote(event) {
         event.preventDefault()
         const form = new FormData(event.target)
+        const image = form.get("image")
         const data = {
             name: form.get("name"),
             description: form.get("description"),
+            image: image.name,
         }
+        if (!!data.image) await Storage.put(data.name, image)
         await API.graphql({
             query: createNoteMutation,
             variables: { input: data },
@@ -50,15 +63,15 @@ const App = ({ signOut }) => {
         event.target.reset()
     }
 
-    async function deleteNote({ id }) {
+    async function deleteNote({ id, name }) {
         const newNotes = notes.filter((note) => note.id !== id)
         setNotes(newNotes)
+        await Storage.remove(name)
         await API.graphql({
             query: deleteNoteMutation,
             variables: { input: { id } },
         })
     }
-
     return (
         <View className="App">
             <Heading level={1}>My Notes App</Heading>
@@ -98,6 +111,12 @@ const App = ({ signOut }) => {
                             {note.name}
                         </Text>
                         <Text as="span">{note.description}</Text>
+                        <View
+                            name="image"
+                            as="input"
+                            type="file"
+                            style={{ alignSelf: "end" }}
+                        />
                         <Button
                             variation="link"
                             onClick={() => deleteNote(note)}
